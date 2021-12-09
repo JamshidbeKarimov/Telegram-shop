f3eeb41ee96f41e653409543a65b517f662d5aad
ObidjonEshmamatov2000 <77581543+ObidjonEshmamatov2000@users.noreply.github.com>
2021-12-09 12:19:33 +0500

    user repo and user service added

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,45 @@
+package service;
+
+import enums.RoleUser;
+import model.User;
+import response.UserRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class UserService extends UserRepository {
+    @Override
+    public User get(UUID id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getList() {
+        return null;
+    }
+
+    @Override
+    public List<User> getList(UUID id) {
+        return null;
+    }
+
+    @Override
+    public String add(User user) {
+        return null;
+    }
+
+    @Override
+    public String editById(UUID id, User user) {
+        return null;
+    }
+
+    @Override
+    public String toggleActivation(UUID id) {
+        return null;
+    }
+
+    @Override
+    protected List<User> getUsers(RoleUser roleUser) {
+        return null;
+    }
+}
